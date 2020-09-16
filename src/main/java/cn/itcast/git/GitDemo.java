9d6ccdc1fb96db51b379a35fe06ee9625baf2232
mengduo <44640490+HOPE6606@users.noreply.github.com>
2020-09-16 21:18:50 +0800

    Update GitDemo.java

--- src/main/java/cn/itcast/git/GitDemo.java
+++ src/main/java/cn/itcast/git/GitDemo.java
@@ -2,7 +2,7 @@ package cn.itcast.git;
 
 public class GitDemo {
     public static void main(String[] args) {
-        System.out.println("Hello Git  ooooo  shabi  lkasmglkamgklm   kmvfiemokglmjniudnlkjasngknuoinaigniuae");
+        System.out.println("Hello Git  ooooo  shabi  lkasmglkamgklm   kmvfiemokglmjniudnlkjasngknuoinaigniuae   08w4u08tyujw9jbondaskng");
 
     }
 }
